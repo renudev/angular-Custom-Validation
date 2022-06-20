400d81b2c0b48d90e9be1d842aeda830dfa8baa9
renudev <renukasd1992@gmail.com>
2022-06-21 00:58:32 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,54 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from './Helper/must-match.validator';
+// import { MustMatch } from 'src/Helper/must-match.validator';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
+export class AppComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-  export class AppComponent implements OnInit {
-    registerForm: FormGroup;
-    submitted = false;
+  constructor(private formBuilder: FormBuilder) {}
 
-    constructor(private formBuilder: FormBuilder) { }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+       
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue],
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword'),
+      }
+    );
+  }
 
-    ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            title: ['', Validators.required],
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(6)]],
-            confirmPassword: ['', Validators.required],
-            acceptTerms: [false, Validators.requiredTrue]
-        }, {
-            validator: MustMatch('password', 'confirmPassword')
-        });
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
     }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
+    // display form values on success
+    alert(
+      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4)
+    );
+  }
 
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-            return;
-        }
-
-        // display form values on success
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
-    }
-
-    onReset() {
-        this.submitted = false;
-        this.registerForm.reset();
-    }
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
-
